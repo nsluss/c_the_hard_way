1e8c8dfc713d7d81145a2eb39f48f09e64a0b41d
Tyler Brothers <tylerbrothers1@gmail.com>
2014-06-12 10:49:21 -0700

    Fixed exwork.py, playing on ex6.
    For instructions on exwork.py, simple type "python exwork.py help"

--- tylerb/exwork.py
+++ tylerb/exwork.py
@@ -7,22 +7,32 @@ def main(argv):
 		print("2) type: python exwork.py [name of new directory]")
 		print("3) cd to the new  directory. Begin the next lesson of C The Hard Way, Makefile  ready to go!")
 	else:
-		os.mkdir(os.getcwd()+"/"+argv[0])
-		print("Created a new directory at: "+os.getcwd()+"/"+argv[0])
+		newdir = makeDir(os.getcwd(), argv[0])
+		print("Created a new directory at: "+newdir)
 		
-		f = open(argv[0]+'/Makefile','w')
-		for l in makefile():
-			f.write(l)
-		f.close()
+		createMakeFile(newdir)
 		print("Created a new default Makefile in that directory")
-			
-def makefile():
+		
+		#createMakeFile(os.getcwd())
+
+def getFileContents():
 	mk =[
-	"CFLAGS=-Wall -g",
-	"clean:",
-	"     rm -f ex1",
+	"CFLAGS=-Wall -g\n",
+	"clean:\n",
+	"\trm -f ex1",
 	]
 	return mk
+
+def makeDir(cwd,fname):
+	directory = cwd+"/"+fname
+	os.mkdir(directory)
+	return directory
+
+def createMakeFile(dirname):
+	f = open(dirname+'/Makefile','w')
+	for l in getFileContents():
+		f.write(l)
+	f.close()
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
