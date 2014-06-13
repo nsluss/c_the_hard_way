0739f7f68059915d404f32590344afc3b785cda5
Tyler Brothers <tylerbrothers1@gmail.com>
2014-06-12 23:36:04 -0700

    finished ex6

--- tylerb/ex6/ex6.c
+++ tylerb/ex6/ex6.c
@@ -5,8 +5,9 @@ int main(int argc, char *argv[]){
 	float power = 2.345f;
 	double super_power = 56789.4532;
 	char initial = 'A';
-	char first_name[] = 'Zed';
-	char last_name[] = 'Shaw';
+	char first_name[] = "Zed";
+	char last_name[] = "Shaw";
 
-	printf("You have %d levels of power",power);
+	printf("You have %f levels of power",power);
+	printf("lastname: %s",last_name);
 }
